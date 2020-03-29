be054b8cadcf9ea5e84d15cceaa6b4506529b974
Peter Prevos <peter@prevos.net>
2020-03-30 08:25:00 +1100

    Updated repo

--- tetris.r
+++ tetris.r
@@ -3,13 +3,12 @@ library(tidyverse)
 
 tetris_scores <- read.table("~/.emacs.d/games/tetris-scores")
 
-str(tetris_scores)
-
 tetris_scores %>%
-    transmute(DateTime = as.POSIXct(paste(V8, V7, V10, V9), format = "%d %B %Y %T"),
-           Player = paste(V3, V4),
+    transmute(DateTime = as.POSIXct(paste(V4, V3, V6, V5),
+                                    format = "%d %B %Y %T"),
+           Player = paste(V7, V8),
            Score = V1) %>%
     ggplot(aes(DateTime, Score, colour = Player)) +
-        geom_point(col = "blue") +
-        geom_smooth(col = "red") + 
-        facet_wrap(~Player)
+    geom_point(col = "blue", size = 4) +
+    geom_smooth(col = "red") + 
+    facet_wrap(~Player)
