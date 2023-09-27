354667022e8d76eb982327337e3f2cf3163c52d2
Peter Prevos <peter@prevos.net>
2023-09-28 08:09:02 +1000

    Added notes drawer functions

--- modules/ews-writing.el
+++ modules/ews-writing.el
@@ -9,6 +9,25 @@
          ("<f7>" . flyspell-word)
          ("C-;" . flyspell-auto-correct-previous-word)))
 
+;; Notes drawers
+(defun ews-org-insert-notes-drawer ()
+  "Generate a NOTES drawer under the heading of the current or jump to an existing one."
+  (interactive)
+  (push-mark)
+  (org-previous-visible-heading 1)
+  (next-line 1)
+  (if (looking-at-p "^[ \t]*:NOTES:")
+      (progn
+        (re-search-forward "^[ \t]*:END:" nil t)
+        (previous-line)
+        (end-of-line)
+        (org-return))
+    (org-insert-drawer nil "NOTES"))
+  (message "Press C-u C-SPACE to return to previous position."))
+
+(with-eval-after-load "org"
+  (define-key org-mode-map (kbd "C-c C-x n") #'ews-org-insert-notes-drawer))
+
 ;; Auto completion
 (use-package company
   :custom
