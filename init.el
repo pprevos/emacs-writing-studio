fa7a672e838de3a53116bcd9c931540572ea109f
Peter Prevos <peter@prevos.net>
2025-04-20 05:29:14 +0200

    Compatability with Denote 4.0

--- init.el
+++ init.el
@@ -411,18 +411,20 @@
   (dired-mode . denote-dired-mode)
   :custom-face
   (denote-faces-link ((t (:slant italic))))
-  :init
-  (require 'denote-org-extras)
   :bind
   (("C-c w d b" . denote-find-backlink)
    ("C-c w d d" . denote-date)
    ("C-c w d l" . denote-find-link)
-   ("C-c w d h" . denote-org-extras-link-to-heading)
    ("C-c w d i" . denote-link-or-create)
    ("C-c w d k" . denote-rename-file-keywords)
    ("C-c w d n" . denote)
    ("C-c w d r" . denote-rename-file)
    ("C-c w d R" . denote-rename-file-using-front-matter)))
+
+(use-package denote-org
+  :bind
+  (("C-c w d h" . denote-org-link-to-heading)
+   ("C-c w d s" . denote-org-extract-subtree)))
 
 ;; Consult convenience functions
 
